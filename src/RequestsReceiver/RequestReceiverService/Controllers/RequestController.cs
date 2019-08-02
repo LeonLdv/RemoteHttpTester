9bbfcc8bb67d31e9bc376497dee25360599ccb31
liond <bissleo@gmail.com>
2019-08-03 00:11:19 +0300

    Adding basic functionality

---
+++ src/RequestsReceiver/RequestReceiverService/Controllers/RequestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRT.RequestReceiverService.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class RequestController : ControllerBase
+	{
+
+
+		// POST api/Request
+		[HttpPost]
+		public void Post([FromBody] string value)
+		{
+
+		}
+
+	
+	}
+}
